bf2f315e3a1fe4c059617b3225df8867707789b1
Otavio Romao <otavio.romao@inatel.br>
2020-08-22 11:52:15 -0300

    Challenge To Create REST API
    The project offers support for REST API to execute a Get,
    Post, Put, Patch and Delete of characters.
    
    This is a Maven project with Spring Boot Framework.

---
+++ br.challenge.makemagic.character/src/main/java/br/challenge/makemagic/MakeMagicApplication.java
@@ -0,0 +1,17 @@
+//**********************************************************************
+// This is intended to solve a challenge to create a REST API to offers
+// CRUD functionalities for Harry Potter's characters.
+// **********************************************************************
+package br.challenge.makemagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMagicApplication
+{
+    public static void main(String[] args)
+    {
+	SpringApplication.run(MakeMagicApplication.class, args);
+    }
+}
---
+++ br.challenge.makemagic.character/src/main/java/br/challenge/makemagic/ctrl/CharacterController.java
@@ -0,0 +1,75 @@
+//**********************************************************************
+// This is intended to solve a challenge to create a REST API to offers
+// CRUD functionalities for Harry Potter's characters.
+// **********************************************************************
+package br.challenge.makemagic.ctrl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.challenge.makemagic.model.CharacterModel;
+import br.challenge.makemagic.repository.CharacterRepository;
+
+@Controller
+@RequestMapping(path = "/character")
+public class CharacterController {
+
+    @Autowired
+    private CharacterRepository repository;
+
+    @RequestMapping(path = "*")
+    public ResponseEntity<Object> methodsNotAllowed() {
+	return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<CharacterModel>> getCharacterModel() {
+	Iterable<CharacterModel> characters = repository.findAll();
+
+	return new ResponseEntity<>(characters, HttpStatus.OK);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CharacterModel> createCharacter(@RequestBody CharacterModel character) {
+
+	repository.save(character);
+
+	return new ResponseEntity<CharacterModel>(character, HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CharacterModel> patchCharacter(@RequestBody CharacterModel character, @PathVariable Long id) {
+	character.setId(id);
+
+	repository.save(character);
+
+	return new ResponseEntity<CharacterModel>(character, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CharacterModel> putCharacter(@RequestBody CharacterModel character, @PathVariable Long id) {
+	character.setId(id);
+
+	repository.save(character);
+
+	return new ResponseEntity<CharacterModel>(character, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<CharacterModel> deleteCharacter(@PathVariable Long id) {
+	repository.deleteById(id);
+
+	return new ResponseEntity<CharacterModel>(HttpStatus.NO_CONTENT);
+    }
+}
