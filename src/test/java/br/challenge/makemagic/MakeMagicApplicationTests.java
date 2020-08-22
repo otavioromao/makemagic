2c7da3d653e862d241f922d1e8c1367f6904d496
Otavio Romao <otavio.romao@inatel.br>
2020-08-22 11:52:15 -0300

    Challenge To Create REST API
    The project offers support for REST API to execute a Get,
    Post, Put, Patch and Delete of characters.
    
    This is a Maven project with Spring Boot Framework.
    
    And, this is the first import of the project.

---
+++ src/test/java/br/challenge/makemagic/MakeMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.challenge.makemagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
